48e907d981e5be161418ae442960038fcf43191e
ShailenderSingh2013 <shailender.2013@gmail.com>
2022-12-21 23:22:12 +0530

    Added Another test

--- GoogleTests/GoogleTest.cs
+++ GoogleTests/GoogleTest.cs
@@ -29,22 +29,16 @@ namespace GoogleTests
             Assert.AreEqual(BaseComponent.Driver.Title, "Selenium Webdriver - Google Search");
         }
 
-        //[Test]
-        ////[TestCaseSource(typeof(TestBase), "BrowserToRunWith")]
-        //public void TestMethod2()
-        //{
-
-        //    //test = AutomationHelper.Reporter.Reporter.ReportManager.CreateTest("TestMethod").Info("Test Started..");
-        //    BaseComponent.LaunchBrowser("http://google.co.in");
-        //    //BaseComponent.GetElement(Pages.home.SearchTextBox).SendKeys("Selenium Webdriver");
-        //    //Thread.Sleep(5000);
-        //    //BaseComponent.GetElement(Pages.home.SearchButton).Click();
-        //    //Thread.Sleep(5000);
-
-        //    Console.WriteLine();
-
-
-
-        //}
+        [Test]
+        public void TestMethod2()
+        {
+            BaseComponent.LaunchBrowser("http://google.co.in");
+            BaseComponent.GetElement(Pages.home.SearchTextBox).SendKeys("Selenium Tutorial");
+            BaseComponent.GetElement(Pages.home.SearchTextBox).SendKeys(Keys.Tab);
+            Thread.Sleep(5000);
+            BaseComponent.GetDefaultElement(Pages.home.SearchButton).Click();
+            Thread.Sleep(5000);
+            Assert.AreEqual(BaseComponent.Driver.Title, "Selenium Tutorial - Google Search");
+        }
     }
 }
